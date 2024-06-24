eb2ba2fecc27d1b5d82389501360a51c4b0968eb
Joanna <joanna_pinczuk@hotmail.com>
2024-06-24 23:57:43 +0200

    updates

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-playground/",
+  base: "/goit-react-hw-03-image-finder/",
   build: {
     outDir: "./dist",
   },
