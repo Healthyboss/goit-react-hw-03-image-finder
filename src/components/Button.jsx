77dacf7e65b799c417c6aa15a991a8d79bbfc0e4
Joanna <joanna_pinczuk@hotmail.com>
2024-06-27 17:37:59 +0200

    final

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import styles from './Button.module.css'
 
 const Button = ({onClick}) => {
+    return(
     <button type='button' className= {styles.buttonLoadMore} onClick={onClick}>
         Load More
     </button>
+    );
 }
 
 Button.propTypes = {
